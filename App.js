265bce9e738572f966d54084a800c7dcf7e756ab
unknown <boxy3460@gmail.com>
2020-03-10 20:53:16 +0800

    wk1-2

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  let myname =<Text>110719023 史翊芃</Text>
+  return (
+    <View style={styles.container}>
+      <Text>哈囉 RN!</Text>
+      <Text>110719023 史翊芃</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
